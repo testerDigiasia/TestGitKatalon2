9fa94e68ced0f3656a7e751ad0685120a46b9120
fenaldy <fenaldy13@gmail.com>
2018-12-26 16:29:48 +0700

    test msg

--- Scripts/tc by TestGitKatalaon2_/Script1545815318703.groovy
+++ Scripts/tc by TestGitKatalaon2_/Script1545815318703.groovy
@@ -13,3 +13,9 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('lifecharger.org')
+
+WebUI.closeBrowser()
+
---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4c345ab2-ad5c-49a7-8f15-701ecbf76e2d</testSuiteGuid>
+</TestSuiteEntity>
